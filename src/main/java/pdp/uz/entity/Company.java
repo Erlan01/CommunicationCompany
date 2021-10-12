ba3be86e4acd1f9eb6c59deb871ebc86a3880466
erlan01 <sultamuratoverlan@gmail.com>
2021-10-12 22:19:45 +0500

    First commit

---
+++ src/main/java/pdp/uz/entity/Company.java
@@ -0,0 +1,26 @@
+package pdp.uz.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "company")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+}
